7a1051a678208c415435b3555c926a281cd238d9
Ca3al0 <lopexangel24@gmai.com>
2025-10-05 23:21:05 -0500

    modified:   __pycache__/config.cpython-313.pyc 	modified:   app.py 	modified:   config.py 	modified:   templates/emails/layout/email_base.html 	modified:   templates/emails/verification_success.html 	modified:   templates/emails/welcome_verification.html

--- config.py
+++ config.py
@@ -6,15 +6,14 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Clave secreta para la seguridad de la aplicación
-    # Usa una variable de entorno para producción
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'una-llave-secreta-para-proteger-las-sesiones'
 
     # -------------------------------------------------------------
-    # Configuración de Flask-Mail para el envío de correos
+    # ✅ CONFIGURACIÓN ACTUALIZADA PARA EL NUEVO CORREO
     # -------------------------------------------------------------
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'jsuarezhe04@gmail.com'
-    MAIL_PASSWORD = 'qutjgxhnangevraz'
-    MAIL_DEFAULT_SENDER = 'jsuarezhe04@gmail.com'
+    MAIL_USERNAME = 'Gestion.Acentrax@gmail.com'
+    MAIL_PASSWORD = 'bgycesijhhnoqhac'  
+    MAIL_DEFAULT_SENDER = 'Gestion.Acentrax@gmail.com'
